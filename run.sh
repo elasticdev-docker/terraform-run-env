16a38029eef954f4fdf5569d09e11465181e7e50
Gary <gary@elasticdev.io>
2019-11-26 13:44:54 -0800

    update

--- run.sh
+++ run.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
 export METHOD=${METHOD:=create}
+export SHARE_DIR=${SHARE_DIR:=/var/tmp/share}
 
-cd /var/tmp/share || exit 4
+cd $SHARE_DIR || exit 4
 
 if [ $METHOD == "create" ]
 then
     terraform init && \
     terraform plan && \
-    terraform apply -auto-approve
-else
-    terraform destroy -auto-approve
+    terraform apply -auto-approve || exit 9
+    exit 0
 fi
 
+if [ $METHOD == "destroy" ]
+then
+    terraform destroy -auto-approve || exit 9
+    exit 0
+fi
+
+print "WARNING METHOD $METHOD does not fit create or destroy - skipping ..."
