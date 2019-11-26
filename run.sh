e430f8b99325b2d7b0dbed8fad51facb64b1d53e
Gary <gary@elasticdev.io>
2019-11-25 23:49:00 -0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-cd /var/tmp/share
-terraform init && \
-terraform plan && \
-terraform apply -auto-approve
+export METHOD=${METHOD:=create}
+
+cd /var/tmp/share || exit 4
+
+if [ $METHOD == "create" ]
+then
+    terraform init && \
+    terraform plan && \
+    terraform apply -auto-approve
+else:
+    terraform destroy -auto-approve
+fi
+
