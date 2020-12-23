3fab92668aba5e790f6adef1bf5c5c8229e710ec
Gary <gary@elasticdev.io>
2020-12-23 21:46:04 +0800

    update

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 export METHOD=${METHOD:=create}
 export SHARE_DIR=${SHARE_DIR:=/var/tmp/share}
+export EXEC_DIR=${EXEC_DIR:=/var/tmp/share/var/tmp/terraform}
 
 echo ""
 echo "The method is $METHOD"
 echo ""
 
-cd $SHARE_DIR || exit 4
+cd $EXEC_DIR || exit 4
 
 if [ $METHOD == "create" ]
 then
